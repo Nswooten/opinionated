af10562f6b5d43f86fcec51e91fc1f99dba50c5c
Nick Wooten <nswooten@gmail.com>
2023-04-22 17:32:46 -0500

    create new and show view for mediaTypes  with asscociated routes and controllers

--- controllers/mediaTypes.js
+++ controllers/mediaTypes.js
@@ -2,8 +2,6 @@ import { MediaType } from "../models/mediaType.js"
 import { Opinion } from "../models/opinion.js"
 
 function index(req, res) {
-  console.log(req.query.type)
-  
   MediaType.find(req.query)
   .then(mediaTypes => {
     res.render('mediaTypes/index', {
@@ -15,13 +13,50 @@ function index(req, res) {
     console.log(err)
     res.redirect('/movies/new')
   })
-  
 }
 
+function create(req, res){
+  MediaType.create(req.body)
+  .then(mediaType => {
+    res.redirect("/mediaTypes/new")
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/mediaTypes")
+  })
+}
+
+function newMediaType(req, res){
+  res.render('mediaTypes/new.ejs', {
+    title: "Media",
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/mediaTypes")
+  })
+}
+
+function show(req, res){
+  MediaType.findById(req.params.mediaTypeId)
+  .populate('opinions')
+  .then(mediaType => {
+    res.render('mediaTypes/show', {
+      mediaType,
+      title: mediaType.title
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/mediaTypes")
+  })
+}
 
 
 
 
 export{
   index,
+  create,
+  newMediaType as new,
+  show,
 }
--- routes/mediaTypes.js
+++ routes/mediaTypes.js
@@ -4,6 +4,12 @@ import * as mediaTypesCtrl from '../controllers/mediaTypes.js'
 const router = Router()
 
 router.get('/', mediaTypesCtrl.index)
+router.get('/new', mediaTypesCtrl.new)
+router.get('/:mediaTypeId', mediaTypesCtrl.show) 
+
+
+
+router.post("/", mediaTypesCtrl.create)
 
 export {
   router
