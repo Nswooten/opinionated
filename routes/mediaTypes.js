fdeeba0a82928ca1141b7e2e3425d089b56ab5c8
Nick Wooten <nswooten@gmail.com>
2023-04-25 08:26:57 -0500

    add isLoggedIn protection

--- routes/mediaTypes.js
+++ routes/mediaTypes.js
@@ -1,17 +1,18 @@
 import { Router } from 'express'
 import * as mediaTypesCtrl from '../controllers/mediaTypes.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
-router.get('/', mediaTypesCtrl.index)
-router.get('/new', mediaTypesCtrl.new)
-router.get('/:mediaTypeId', mediaTypesCtrl.show)
+router.get('/', isLoggedIn, mediaTypesCtrl.index)
+router.get('/new', isLoggedIn, mediaTypesCtrl.new)
+router.get('/:mediaTypeId', isLoggedIn, mediaTypesCtrl.show)
 
-router.post('/:mediaTypeId/opinions', mediaTypesCtrl.addOpinion)
+router.post('/:mediaTypeId/opinions', isLoggedIn, mediaTypesCtrl.addOpinion)
 
 
 
-router.post("/", mediaTypesCtrl.create)
+router.post("/", isLoggedIn, mediaTypesCtrl.create)
 
 export {
   router
--- routes/profiles.js
+++ routes/profiles.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import * as profilesCtrl from '../controllers/profiles.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
@@ -7,8 +8,8 @@ router.get('/', function (req, res) {
   res.render('index', { title: 'Home Page' })
 })
 
-router.put('/:mediaTypeId/opinions/:opinionId', profilesCtrl.updateOpinion)
-router.delete('/:mediaTypeId/opinions/:opinionId', profilesCtrl.deleteOpinion)
+router.put('/:mediaTypeId/opinions/:opinionId', isLoggedIn, profilesCtrl.updateOpinion)
+router.delete('/:mediaTypeId/opinions/:opinionId', isLoggedIn, profilesCtrl.deleteOpinion)
 export {
   router
 }
